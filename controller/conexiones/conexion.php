b2d3295469c221e6adcfddd4fd07f5938f3877de
= <=>
2018-01-18 08:32:32 -0500

    test

--- controller/conexiones/conexion.php
+++ controller/conexiones/conexion.php
@@ -1,10 +1,10 @@
 <?php
-$username="mipc_20799635";
-$password="000360";
-$database="mipc_20799635_arpMapa";
-$servidor = "sql302.mipropia.com";
+$username="admin";
+$password="admin";
+$database="arpproyecto";
+$servidor ="sql302.mipropia.com";
 ?>
 
 <?php
-$connection = new mysqli($servidor,$username,$password,$database) or die ("Conexion erronea");
+$connection = new mysqli("localhost",$username,$password,$database) or die ("Conexion erronea");
 ?>
